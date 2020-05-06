95a1cb2513c02e2965953014b7c2c64be8634cb7
王伟 <>
2020-05-07 00:37:42 +0800

    update

--- 异常处理/异常处理/Signal.swift
+++ 异常处理/异常处理/Signal.swift
@@ -8,38 +8,77 @@
 
 import Foundation
 
-class Signal<T>: NSObject {
+class Observable<T>: NSObject {
     
-    typealias SignalToken = Int
+    typealias ObservableToken = Int
     typealias Subscriber = (T) -> Void
     
     var tokenCounter :Int = 0
     internal private(set) var value : T?
-    var subscribers :Dictionary = [SignalToken: Subscriber]()
+    var subscribers :Dictionary = [ObservableToken: Subscriber]()
     
     let queue = DispatchQueue(label: "com.swift.signal.token")
     
-    init(value: T) {
+    init(pure value: T?) {
         self.value = value
     }
     
-    func subscribe(subscriber: @escaping Subscriber) -> SignalToken {
-        let token = signalNextToken()
+    func subscribe(subscriber: @escaping Subscriber) -> ObservableToken {
+        let token = observableToken()
         subscribers.updateValue(subscriber, forKey: token)
         return token
     }
     
-    func bind(signal: Signal) -> SignalToken {
-        // 将两个Signal进行绑定
-        return 1
+    // 绑定两个Signal的值
+    func bind(signal: Observable) -> ObservableToken {
+        return self.subscribe { x in
+            signal.update(x)
+        }
+    }
+    
+    // bind函数
+    func flatMap<B>(f: @escaping (T)-> Observable<B>) -> Observable<B> {
+        let signal: Observable<B> = Observable<B>(pure: nil)
+        _ = self.subscribe(subscriber: { (x) in
+            let newS = f(x)
+            signal.update(newS.peek())
+            _ = newS.bind(signal: signal)
+        })
+        return signal
     }
     
     func update(_ value: T) {
         _ = subscribers.compactMap { $1(value) }
     }
     
-    func signalNextToken() -> Int {
-        return tokenCounter + 1
+    func peek() -> T {
+        return self.value!
     }
     
+    func observableToken() -> Int {
+        tokenCounter = tokenCounter + 1
+        return tokenCounter
+    }
+    
+}
+
+extension Observable {
+   
+    func map<B>(f: @escaping (T) -> B) -> Observable<B> {
+        let mapObservable = Observable<B>(pure: nil)
+        _ = self.subscribe(subscriber: { x in
+            mapObservable.update(f(x))
+        })
+        return mapObservable
+    }
+    
+    func filter(f: @escaping (T) -> Bool) -> Observable<T> {
+        let filterObservable = Observable<T>(pure: self.value)
+        _ = self.subscribe { x in
+            if f(x) {
+                filterObservable.update(x)
+            }
+        }
+        return filterObservable
+    }
 }
